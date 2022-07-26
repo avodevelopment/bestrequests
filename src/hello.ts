07fb4e770e4c4c9bcf8e6fb6d736d019f227da0f
Bill <bill@ntaltd.co.uk>
2022-07-26 09:51:42 -0300

    Initial typescript setup and hello functions

---
+++ src/hello.ts
@@ -0,0 +1,7 @@
+export function sum(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+export function hello(name: string) {
+    console.log(`hello ${name}!`);
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export {sum, hello} from './hello';
